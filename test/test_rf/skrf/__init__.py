e53dff6a70862a2bb186c7bb55f63f7958ca8f40
cowboy_niu <q384413898@163.com>
2026-07-13 16:40:18 +0800

    chore: 批量新增大量测试与库文件
    - 新增多个VNA仪器驱动实现，包括Keysight、NanoVNA、Rohde & Schwarz、HP系列
    - 新增校准、媒体库、IO工具等核心模块实现
    - 新增测试数据文件、绘图样式文件与工具脚本
    - 更新.gitignore配置

---
+++ test/test_rf/skrf/__init__.py
@@ -0,0 +1,69 @@
+"""
+skrf is an object-oriented approach to microwave engineering,
+implemented in Python.
+"""
+
+__version__ = '2.0.1'
+# Import all  module names for coherent reference of name-space
+import os as _os
+
+from . import (
+    calibration,
+    circuit,
+    constants,
+    data,
+    frequency,
+    instances,
+    io,
+    mathFunctions,
+    media,
+    network,
+    networkSet,
+    plotting,
+    qfactor,
+    taper,
+    tlineFunctions,
+    util,
+    vectorFitting,
+    vi,
+)
+from .calibration import calibrationSet, deembedding
+from .frequency import Frequency
+from .network import Network
+from .plotting import stylely
+
+
+def setup_plotting():
+    plotting_environment = _os.environ.get('SKRF_PLOT_ENV', "pylab").lower()
+    if plotting_environment == "pylab-skrf-style":
+        stylely()
+    # elif some different plotting environment
+        # set that up
+
+
+setup_plotting()
+
+__all__ = [
+    "calibration",
+    "calibrationSet",
+    "deembedding",
+    "circuit",
+    "constants",
+    "data",
+    "frequency",
+    "Frequency",
+    "Network",
+    "instances",
+    "io",
+    "mathFunctions",
+    "media",
+    "network",
+    "networkSet",
+    "plotting",
+    "qfactor",
+    "taper",
+    "tlineFunctions",
+    "util",
+    "vectorFitting",
+    "vi",
+]
